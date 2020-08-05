66fc1bd98b61665548fb6f2982ae2697de94e738
Shekhar Dhakal <shekhardhakal2015@gmail.com>
2020-08-05 09:04:38 +0545

    second commit

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Home(models.Model):
+   
+    caption =models.CharField(max_length=255)
+    video_link = models.CharField(max_length=255)
+    slider_image =models.ImageField(upload_to='media/home',null=True)
+
+class About(models.Model):
+   
+    total_project =models.CharField(max_length=255)
+    about_description = models.TextField()
+    about_image =models.ImageField(upload_to='media/about',null=True)
+
+class Skill(models.Model):
+   
+    skill_title =models.CharField(max_length=255)
+    skill_progress = models.CharField(max_length=255)
+
+class Project(models.Model):
+   
+    spam_text =models.CharField(max_length=255)
+    project_titel = models.CharField(max_length=255)
+    project_image =models.ImageField(upload_to='media/projects',null=True)
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
+from apps.core.models import Home,Skill,Project,About
+from django.shortcuts import render,get_object_or_404
+from django.views.generic.list import ListView
+from django.conf import settings
+
+# Create your views here.
+class HomeList(ListView):
+    model = Home
+    queryset = Home.objects.all()
+    context_object_name = 'list_slider'
+    template_name='partials/homeslider.html'
+
+class SkillList(ListView):
+    model = Skill
+    queryset = Skill.objects.all()
+    context_object_name = 'list_skill'
+    template_name='partials/skill.html'
+
+class ProjectList(ListView):
+    model = Project
+    queryset = Project.objects.all()
+    context_object_name = 'list_project'
+    template_name='partials/project.html'
+
+class AboutList(ListView):
+    model = About
+    queryset = About.objects.all()
+    context_object_name = 'about'
+    template_name='partials/about.html'
+
 
 # Create your views here.
 def home(request):
     template_name='index.html'
     return render(request, template_name)
+
+
